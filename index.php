4a9b03ba4677ec31d240dbd8b5a773bc43ea2aed
Matthias Subileau <dskillz@protonmail.com>
2024-04-07 15:42:49 +0200

    Cleaning some errors - Definitive version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
     <title>WebAgency</title>
-    <link rel="icon" href="img/favicon.ico">
+    <link rel="icon" href="/img/favicon.ico">
 
     <meta name="description" content="Tout le Webdesign, solutions clés en main, rapport qualité/prix garanti">
     <meta name="keywords" content="webdesign, solutions clés en main">
@@ -13,14 +13,12 @@
     <meta name="author" content="Subileau Matthias">
     <meta name="robots" content="index, follow">
 
-    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-    <link rel="stylesheet" type="text/css" href="CSS/fontawesome.min.css">
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" type="text/css" href="/CSS/styles.css">
+    <link rel="stylesheet" type="text/css" href="/CSS/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto">
     <!--<link rel="stylesheet" type="text/css" href="onepage-scroll.css">-->
 
-    <script
-        src="js/jquery.min.js">
+    <script src="/js/jquery.min..js">
     </script>
 
 </head>
@@ -28,7 +26,7 @@
 <body cz-shortcut-listen="true">
 
     <header>
-        <img src="img/logo.png" alt="logo">
+        <img src="/img/logo.png" alt="logo">
         <nav>
             <ul>
                 <li><a href="#Accueil">Accueil</a></li>
@@ -43,14 +41,14 @@
             <div id="Slider">
                 <h1><strong>WEBAGENCY</strong> : L'AGENCE DE TOUS<br> VOS PROJETS !</h1>
                 <figure class="visible">
-                    <img src="img/bg1.jpg">
+                    <img src="/img/bg1.jpg">
                     <figcaption>
                         <p>Vous avez un projet web ? La WebAgency vous aide à le réaliser !</p>
                         <a href="#" class="generic-button">Plus d'infos</a>
                     </figcaption>
                 </figure>
                 <figure>
-                    <img src="img/bg2.jpg">
+                    <img src="/img/bg2.jpg">
                     <figcaption>
                         <p>Vous avez un projet web ? La WebAgency vous aide à le réaliser !</p>
                         <a href="#" class="generic-button">Plus d'infos</a>
@@ -67,8 +65,7 @@
             <h1>NOS SERVICES</h1>
             <p>Voici les services que vous proposent la WebAgency pour la création vos sites web</p>
             <div class="flex1">
-                <img src="img/main-feature.png"
-                    alt="logo de présentation ordinateur">
+                <img src="/img/main-feature.png" alt="logo de présentation ordinateur">
                 <div id="ServicesList">
                     <ul>
                         <li> <i class="fa fa-line-chart"></i>
@@ -90,83 +87,70 @@
         </section>
 
         <section id="Portfolio">
-            <h1>NOS PROJETS</h1>
-            <p>Voici quelques exemples de sites que nous avons réalisé pour nos clients</p>
-            <div class="flex2">
-                <ul>
-                    <li class="selected"><button>Tous</button> </li>
-                    <li><button>Créatif</button></li>
-                    <li><button>Entreprise</button></li>
-                    <li><button>Portfolio</button></li>
-                </ul>
+            <div>
+                <h1>NOS PROJETS</h1>
+                <p>Voici quelques exemples de sites que nous avons réalisé pour nos clients</p>
+                <div class="flex2">
+                    <ul>
+                        <li class="selected"><button>Tous</button> </li>
+                        <li><button>Créatif</button></li>
+                        <li><button>Entreprise</button></li>
+                        <li><button>Portfolio</button></li>
+                    </ul>
+                </div>
             </div>
 
-            <div id="portfolio">
+
+            <div class="portfolio">
 
                 <figure>
-                    <a href="#"><img src="img/nose-in-the-sky.jpg"
-                            alt="exemple de creation de site 1"></a>
+                    <a href="#"><img src="/img/nose-in-the-sky.jpg" alt="exemple de creation de site 1"></a>
                     <a href="#">
-                        <figcaption><em>Nose in the sky</em><br>Coach en Relooking<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>Nose in the sky</em><br>Coach en Relooking<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img src="img/pink-rabbit.jpg"
-                            alt="exemple de creation de site 2"></a>
+                    <a href="#"><img src="/img/pink-rabbit.jpg" alt="exemple de creation de site 2"></a>
                     <a href="#">
                         <figcaption><em>Pink Rabbit</em><br>Ingénieurs<i class="fa fa-eye" aria-hidden="true"></i>
                         </figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img src="img/pompadour.jpg"
-                            alt="exemple de creation de site 3"></a>
+                    <a href="#"><img src="/img/pompadour.jpg" alt="exemple de creation de site 3"></a>
                     <a href="#">
-                        <figcaption><em>Pompadour</em><br>Fabricant de chaussures<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>Pompadour</em><br>Fabricant de chaussures<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img
-                            src="img/mysterious-thing.jpg"
-                            alt="exemple de creation de site 4"></a>
+                    <a href="#"><img src="/img/mysterious-thing.jpg" alt="exemple de creation de site 4"></a>
                     <a href="#">
                         <figcaption><em>Mysterious Thing</em><br>Architectes<i class="fa fa-eye" aria-hidden="true"></i>
                         </figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img
-                            src="img/psd-shopping-bag.jpg"
-                            alt="exemple de creation de site 5"></a>
+                    <a href="#"><img src="/img/psd-shopping-bag.jpg" alt="exemple de creation de site 5"></a>
                     <a href="#">
-                        <figcaption><em>PSD Shopping Bag</em><br>Fabricant de shopping bag<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>PSD Shopping Bag</em><br>Fabricant de shopping bag<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img src="img/build-mix.jpg"
-                            alt="exemple de creation de site 6"></a>
+                    <a href="#"><img src="/img/build-mix.jpg" alt="exemple de creation de site 6"></a>
                     <a href="#">
-                        <figcaption><em>Build Mix</em><br>Constructeur en tout genre<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>Build Mix</em><br>Constructeur en tout genre<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img src="img/roquet-inc.jpg"
-                            alt="exemple de creation de site 7"></a>
+                    <a href="#"><img src="/img/roquet-inc.jpg" alt="exemple de creation de site 7"></a>
                     <a href="#">
-                        <figcaption><em>Roquet Inc</em><br>Fabricant de stylo<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>Roquet Inc</em><br>Fabricant de stylo<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
                 <figure>
-                    <a href="#"><img src="img/be-them.jpg"
-                            alt="exemple de creation de site 8"></a>
+                    <a href="#"><img src="/img/be-them.jpg" alt="exemple de creation de site 8"></a>
                     <a href="#">
-                        <figcaption><em>Be Them</em><br>Fournisseur en bureautique<i class="fa fa-eye"
-                                aria-hidden="true"></i></figcaption>
+                        <figcaption><em>Be Them</em><br>Fournisseur en bureautique<i class="fa fa-eye" aria-hidden="true"></i></figcaption>
                     </a>
                 </figure>
             </div>
@@ -176,14 +160,10 @@
 
         <section id="Contact">
             <div id="map">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2156022977924!2d2.347635615869715!3d48.87316630761998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e145cb52f15%3A0x4d3ec41b2cadff0!2s25+Rue+d'Hauteville%2C+75010+Paris!5e0!3m2!1sfr!2sfr!4v1491224661899"
-                    style="border:0" allowfullscreen="" data-ruffle-polyfilled=""></iframe> <!-- google map -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2156022977924!2d2.347635615869715!3d48.87316630761998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e145cb52f15%3A0x4d3ec41b2cadff0!2s25+Rue+d'Hauteville%2C+75010+Paris!5e0!3m2!1sfr!2sfr!4v1491224661899" style="border:0" allowfullscreen="" data-ruffle-polyfilled=""></iframe> <!-- google map -->
                 <div id="Formulaire">
                     <h1>Contact info</h1>
-                    <p><mark>WebAgency SAS</mark><br><a
-                            href="https://www.google.com/maps/search/?api=1&amp;query=25+Rue+Hauteville+75010+Paris"
-                            target="_blank"></a> 25 Rue d'Hauteville 75010 Paris<br>Tel : 01 02 03 04 05</p>
+                    <p><mark>WebAgency SAS</mark><br><a href="https://www.google.com/maps/search/?api=1&amp;query=25+Rue+Hauteville+75010+Paris" target="_blank"></a> 25 Rue d'Hauteville 75010 Paris<br>Tel : 01 02 03 04 05</p>
                     <form method="post" action="traitement.php">
                         <p><input type="text" name="pseudo" id="pseudo" placeholder="Nom" size="35" maxlength="50"></p>
                         <p><input type="email" name="mail" id="mail" placeholder="Email" size="35" maxlength="50"></p>
@@ -199,11 +179,11 @@
 
         <script>
             var $root = $('html, body');
-            $('a').click(function () {
+            $('a').click(function() {
                 var href = $.attr(this, 'href');
                 $root.animate({
                     scrollTop: $(href).offset().top
-                }, 800, function () {
+                }, 800, function() {
                     window.location.hash = href;
                 });
                 return false;
